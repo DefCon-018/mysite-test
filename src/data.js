ae2f0817977324be3788215d0768c8c2a5e0a0e0
DefCon-018 <hp761570@gmil.com>
2021-06-20 19:12:19 +0530

    Setup the directory structure

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        name: 'Management Team',
+        accessLevel : 'All Access',
+        num_members: 4,
+    },
+    {
+        name: 'Procurement Team',
+        accessLevel : 'Restricted Access',
+        num_members: 8,
+    },
+    {
+        name: 'Project Team',
+        accessLevel : 'Restricted Access',
+        num_members: 16,
+    },
+    {
+        name: 'IT Team',
+        accessLevel : 'Restricted Access',
+        num_members: 4,
+    },
+]
