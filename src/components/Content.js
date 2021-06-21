f2fe2b8acfe72823621653bdddf6e5b31b70fe4f
DefCon-018 <hp761570@gmil.com>
2021-06-21 10:50:34 +0530

    Add style to the content Component

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { addData } from '../actions';
 import { data } from '../data';
 import {connect} from 'react-redux';
-import {FaSyncAlt, FaWifi, FaBorderAll} from "react-icons/fa";
+import {FaSyncAlt, FaWifi, FaBorderAll, FaPlus, FaPen, FaTrashAlt, FaRegEye} from "react-icons/fa";
 
 class Content extends React.Component {
 
@@ -12,6 +12,7 @@ class Content extends React.Component {
     }
 
     render() {
+        let {data} = this.props;
         return (
             <div className= "container">
                 <div className= "heading">
@@ -25,7 +26,43 @@ class Content extends React.Component {
                 </div>
 
                 <div className= "content">
-                    
+                    <div className= "content-head">
+                        <button className= "btn"><span className= "plus"><FaPlus /></span> Add Role</button>
+                        <div><FaPen /></div>
+                        <div><FaTrashAlt /></div>
+                    </div>
+                    <div className= "content-detail">
+                        <table>
+                            <tr className= "detail-head">
+                                <td></td>
+                                <td>Department/Role Name</td>
+                                <td className= "access-lvl">Access Level</td>
+                                <td>No. of members</td>
+                                <td>Last updated</td>
+                                <td></td>
+                            </tr>
+                            {
+                                data.map((d, index) =>{
+                                    return (
+                                        <tr>
+                                            <td className= "check">
+                                                <input type= "checkbox" className= "checkbox-round" />
+                                            </td>
+                                            <td>{d.name}</td>
+                                            <td>
+                                                <div className= "access restricted">
+                                                    <p>{d.accessLevel}</p>
+                                                </div>
+                                            </td>
+                                            <td className="center-1">{d.num_members}</td>
+                                            <td className= "center">1 min ago</td>
+                                            <td className= "eye"><FaRegEye /></td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </table>
+                    </div>
                 </div>
             </div>
         );
